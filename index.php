8f553a7e694c2906522c15a97729b721bc23ad20
Albin Kjellberg <albin.kjellberg2000@gmail.com>
2020-11-18 22:08:06 +0100

    commit first

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+   $title = "index";
+   include 'incl/header.php'; 
+?>
+
+<div class="content">
+   <img src="images/albin-kjellberg.png" alt="Albin Kjellberg">
+</div>
+<div class="content" id="top-content-right">
+   <h1>Albin Kjellberg</h1>
+   <p>
+      Jag är en 20 årig kille från <span class="highlight">Stockholm</span>, nu bosatt i Norrköping, som länge varit intresserad av det mesta inom IT.
+      Med grund till detta så bestämde jag mig för att gå gymnasieprogrammet Teknik - <span class="highlight">information- 
+      och medieteknik</span>,
+      vilket har gett mig en ganska stabil grund för webbutveckling och programmering.
+      <br><br>
+      Min fritid består oftast av serietittande, datorspel och träning (tidigare gym men testar lite nya grejer här i Norrköping). 
+      Jag är även självlärd inom <span class="highlight">spelutveckling</span>, vilket är något jag gillar ständigt försöker förbättra 
+      mig själv inom.
+      <br><br>
+      Här är några länkar som representerar mig:
+   </p>
+   <ul>
+      <li><a href="https://unity.com" target="blank">unity.com</a></li>
+      <li><a href="https://nintendo.com" target="blank">nintendo.com</a></li>
+      <li><a href="https://jonbellion.com" target="blank">jonbellion.com</a></li>
+   </ul>
+</div>
+
+<?php include 'incl/footer.php'; ?>
---
+++ js/script.js
@@ -0,0 +1,14 @@
+currentMode = 'dark';
+lightModeImg = [ 'images/moon.svg', 'moon' ];
+darkModeImg = [ 'images/sun.svg', 'sun' ];
+
+var today;
+var h;
+var min;
+var sec;
+hourP = document.getElementById('h');
+minP = document.getElementById('min');
+secP = document.getElementById('sec');
+
+// Start clock
+startClock();
