9a0fc4366fb00de4fb9cbd50ffd4c6d2ec9006e2
Albin Kjellberg <albin.kjellberg2000@gmail.com>
2020-11-19 11:02:13 +0100

    c

--- incl/functions.php
+++ incl/functions.php
@@ -2,13 +2,32 @@
 
 function displaySet($link, $setID) {
    try {
-      $set	=	mysqli_query($link, "SELECT * FROM sets WHERE setId LIKE '10199-1'");
+      // WHERE setId LIKE '10199-1'
+      $set	=	mysqli_query($link, "SELECT * FROM sets");
       while	($row	=	mysqli_fetch_array($set))	{
          $setId =	$row['SetID'];	
          $catId =	$row['CatID'];
          $name = $row['Setname'];
          $year = $row['Year'];
-         echo "<h2>" . $name . " - " . $setId . "</h2>";
+         //echo "<h2>" . $name . " - " . $setId . "</h2>";
+
+         echo 
+            "<table>
+               <tr>
+                  <td>"
+                     . $setId .
+                  "</td>
+                  <td>"
+                     . $name .
+                  "</td>
+                  <td>"
+                     . $catId .
+                  "</td>
+                  <td>"
+                     . $year .
+                  "</td>
+               </tr>
+            </table>"
       }
    } catch (\Throwable $th) {
       echo $set . "<br>" . $th->getMessage();
