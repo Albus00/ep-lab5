c3556073f2c7602436eca4564918bce50d238564
Albin Kjellberg <albin.kjellberg2000@gmail.com>
2020-11-19 10:57:22 +0100

    commit

--- incl/functions.php
+++ incl/functions.php
@@ -1,3 +1,18 @@
-function findSet($link, $setID) {
-   
+<?php
+
+function displaySet($link, $setID) {
+   try {
+      $set	=	mysqli_query($link, "SELECT * FROM sets WHERE setId LIKE '10199-1'");
+      while	($row	=	mysqli_fetch_array($set))	{
+         $setId =	$row['SetID'];	
+         $catId =	$row['CatID'];
+         $name = $row['Setname'];
+         $year = $row['Year'];
+         echo "<h2>" . $name . " - " . $setId . "</h2>";
+      }
+   } catch (\Throwable $th) {
+      echo $set . "<br>" . $th->getMessage();
+   }
 }
+
+?>
--- index.php
+++ index.php
@@ -3,12 +3,10 @@
    include 'incl/functions.php';
    include 'incl/header.php';
 
-   $set	=	mysqli_query($link,	"SELECT	*	FROM	inventory WHERE setId LIKE '10199-1'");																																	
-	while	($row	=	mysqli_fetch_array($set))	{
-      findSet($link, $setID);
-      $setId =	$row['SetID'];	
-      $itemId =	$row['ItemID'];	
-      echo $setId . " ". $itemId .  "<br>";
+   $inventory	=	mysqli_query($link,	"SELECT	*	FROM	inventory WHERE setId LIKE '10199-1'");																																	
+	while	($row	=	mysqli_fetch_array($inventory))	{
+      displaySet($link, $inventory);
+      
    }
 ?>
 <div class="content" id="">
