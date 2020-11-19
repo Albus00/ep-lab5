a535e9e4cea9effd2aeb06868531bb3d23250610
Albin Kjellberg <albin.kjellberg2000@gmail.com>
2020-11-19 11:10:45 +0100

    c

--- incl/functions.php
+++ incl/functions.php
@@ -1,38 +1,38 @@
 <?php
 
 function displaySet($link, $setID) {
-   try {
-      echo "<table>";
+   // try {
+   //    echo "<table>";
 
-      // WHERE setId LIKE '10199-1'
-      $set	=	mysqli_query($link, "SELECT * FROM sets");
-      while	($row	=	mysqli_fetch_array($set))	{
-         $setId =	$row['SetID'];	
-         $catId =	$row['CatID'];
-         $name = $row['Setname'];
-         $year = $row['Year'];
-         //echo "<h2>" . $name . " - " . $setId . "</h2>";
+   //    // WHERE setId LIKE '10199-1'
+   //    $set	=	mysqli_query($link, "SELECT * FROM sets");
+   //    while	($row	=	mysqli_fetch_array($set))	{
+   //       $setId =	$row['SetID'];	
+   //       $catId =	$row['CatID'];
+   //       $name = $row['Setname'];
+   //       $year = $row['Year'];
+   //       //echo "<h2>" . $name . " - " . $setId . "</h2>";
 
-         echo 
-            "<tr>
-               <td>"
-                  . $setId .
-               "</td>
-               <td>"
-                  . $name .
-               "</td>
-               <td>"
-                  . $catId .
-               "</td>
-               <td>"
-                  . $year .
-               "</td>
-            </tr>";
-      }
-   } catch (\Throwable $th) {
-      echo $set . "<br>" . $th->getMessage();
-   }
-   echo "</table>";
+   //       echo 
+   //          "<tr>
+   //             <td>"
+   //                . $setId .
+   //             "</td>
+   //             <td>"
+   //                . $name .
+   //             "</td>
+   //             <td>"
+   //                . $catId .
+   //             "</td>
+   //             <td>"
+   //                . $year .
+   //             "</td>
+   //          </tr>";
+   //    }
+   // } catch (\Throwable $th) {
+   //    echo $set . "<br>" . $th->getMessage();
+   // }
+   // echo "</table>";
 }
 
 ?>
--- index.php
+++ index.php
@@ -3,11 +3,44 @@
    include 'incl/functions.php';
    include 'incl/header.php';
 
-   $inventory	=	mysqli_query($link,	"SELECT	*	FROM	inventory WHERE setId LIKE '10199-1'");																																	
-	while	($row	=	mysqli_fetch_array($inventory))	{
-      displaySet($link, $inventory);
+   // $inventory	=	mysqli_query($link,	"SELECT	*	FROM	inventory WHERE setId LIKE '10199-1'");																																	
+	// while	($row	=	mysqli_fetch_array($inventory))	{
+   //    displaySet($link, $inventory);
       
+   // }
+   
+   try {
+      echo "<table>";
+
+      // WHERE setId LIKE '10199-1'
+      $set	=	mysqli_query($link, "SELECT * FROM sets");
+      while	($row	=	mysqli_fetch_array($set))	{
+         $setId =	$row['SetID'];	
+         $catId =	$row['CatID'];
+         $name = $row['Setname'];
+         $year = $row['Year'];
+         //echo "<h2>" . $name . " - " . $setId . "</h2>";
+
+         echo 
+            "<tr>
+               <td>"
+                  . $setId .
+               "</td>
+               <td>"
+                  . $name .
+               "</td>
+               <td>"
+                  . $catId .
+               "</td>
+               <td>"
+                  . $year .
+               "</td>
+            </tr>";
+      }
+   } catch (\Throwable $th) {
+      echo $set . "<br>" . $th->getMessage();
    }
+   echo "</table>";
 ?>
 <div class="content" id="">
 
