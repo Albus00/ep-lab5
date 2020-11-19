811821dfa328f7a9107736afb0d9605722f52ed3
Albin Kjellberg <albin.kjellberg2000@gmail.com>
2020-11-19 11:25:52 +0100

    c

--- incl/functions.php
+++ incl/functions.php
@@ -1,38 +1,28 @@
 <?php
 
-function displaySet($link, $setID) {
-   // try {
-   //    echo "<table>";
+function displaySet($link, $setId) {
+   $set	=	mysqli_query($link, "SELECT * FROM sets WHERE setID LIKE '" . $setId . "'");
+   while	($row	=	mysqli_fetch_array($set))	{
+      $name = $row['Setname'];
+      $year = $row['Year'];
 
-   //    // WHERE setId LIKE '10199-1'
-   //    $set	=	mysqli_query($link, "SELECT * FROM sets");
-   //    while	($row	=	mysqli_fetch_array($set))	{
-   //       $setId =	$row['SetID'];	
-   //       $catId =	$row['CatID'];
-   //       $name = $row['Setname'];
-   //       $year = $row['Year'];
-   //       //echo "<h2>" . $name . " - " . $setId . "</h2>";
+      echo "<h2>" . $name . " - " . $setId . "</h2>";
+      echo "<h3>" . $year . "</h3>";
+   }
+}
 
-   //       echo 
-   //          "<tr>
-   //             <td>"
-   //                . $setId .
-   //             "</td>
-   //             <td>"
-   //                . $name .
-   //             "</td>
-   //             <td>"
-   //                . $catId .
-   //             "</td>
-   //             <td>"
-   //                . $year .
-   //             "</td>
-   //          </tr>";
-   //    }
-   // } catch (\Throwable $th) {
-   //    echo $set . "<br>" . $th->getMessage();
-   // }
-   // echo "</table>";
+function displaySetInventory($link, $setId) {
+   try {
+      echo "<table>";
+
+      $inventory	=	mysqli_query($link,	"SELECT	*	FROM	inventory WHERE setId LIKE '" . $setId . "'");
+      while	($row	=	mysqli_fetch_array($inventory))	{
+         
+      }
+      echo "</table>";
+   } catch (\Throwable $th) {
+      echo $inventory . "<br>" . $th->getMessage();
+   }
 }
 
 ?>
--- index.php
+++ index.php
@@ -4,47 +4,15 @@
    include 'incl/header.php';
 
    $setId = "10179-1";
-
-   // $inventory	=	mysqli_query($link,	"SELECT	*	FROM	inventory WHERE setId LIKE '10199-1'");																																	
-	// while	($row	=	mysqli_fetch_array($inventory))	{
-   //    displaySet($link, $inventory);
-      
-   // }
    
-   try {
-      echo "<table>";
-
-      // WHERE setId LIKE '10199-1'
-      $set	=	mysqli_query($link, "SELECT * FROM sets WHERE setID LIKE '" . $setId . "'");
-      while	($row	=	mysqli_fetch_array($set))	{
-         $catId =	$row['CatID'];
-         $name = $row['Setname'];
-         $year = $row['Year'];
-         //echo "<h2>" . $name . " - " . $setId . "</h2>";
-
-         echo 
-            "<tr>
-               <td>"
-                  . $setId .
-               "</td>
-               <td>"
-                  . $name .
-               "</td>
-               <td>"
-                  . $catId .
-               "</td>
-               <td>"
-                  . $year .
-               "</td>
-            </tr>";
-      }
-   } catch (\Throwable $th) {
-      echo $set . "<br>" . $th->getMessage();
-   }
-   echo "</table>";
 ?>
-<div class="content" id="">
+<div class="content">
 
+<?php 
+   displaySet($link, $setId);
+   echo "<br><br>";
+   //displaySetInventory($link, $setId);
+?>
    
 </div>
 
