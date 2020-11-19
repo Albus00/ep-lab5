ad428adbdca4f8f9ca646fd1d17cb3b23b8db517
Albin Kjellberg <albin.kjellberg2000@gmail.com>
2020-11-19 10:44:36 +0100

    c

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
    $title = "index";
    include 'incl/header.php';
 
-   $set	=	mysqli_query($link,	"SELECT	*	FROM	inventory");																																	
+   $set	=	mysqli_query($link,	"SELECT	*	FROM	inventory WHERE setId LIKE '10199-1'");																																	
 	while	($row	=	mysqli_fetch_array($set))	{
       $setId =	$row['SetID'];	
       $itemId =	$row['ItemID'];	
