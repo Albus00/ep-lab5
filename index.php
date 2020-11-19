76827ba95eba44155dd995966581c172133b07eb
Albin Kjellberg <albin.kjellberg2000@gmail.com>
2020-11-19 10:38:47 +0100

    commit

--- index.php
+++ index.php
@@ -2,15 +2,14 @@
    $title = "index";
    include 'incl/header.php';
 
-   $set	=	mysqli_query($connection,	"SELECT	*	FROM	inventory");																																	
+   $set	=	mysqli_query($link,	"SELECT	*	FROM	inventory");																																	
 	while	($row	=	mysqli_fetch_array($set))	{
-      $setId	=	$row['SetID'];	
+      $setId =	$row['SetID'];	
       echo $setId . "<br>";
    }
 ?>
 <div class="content" id="">
 
-   hello?
    
 </div>
 
