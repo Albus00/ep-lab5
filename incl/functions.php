523262e0a75070e96149fa60304140e72486a361
Albin Kjellberg <albin.kjellberg2000@gmail.com>
2020-11-19 10:50:16 +0100

    co

---
+++ incl/functions.php
@@ -0,0 +1,3 @@
+function findSet($link, $setID) {
+   
+}
--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
    $title = "index";
+   include 'incl/functions.php';
    include 'incl/header.php';
 
    $set	=	mysqli_query($link,	"SELECT	*	FROM	inventory WHERE setId LIKE '10199-1'");																																	
 	while	($row	=	mysqli_fetch_array($set))	{
+      findSet($link, $setID);
       $setId =	$row['SetID'];	
       $itemId =	$row['ItemID'];	
       echo $setId . " ". $itemId .  "<br>";
